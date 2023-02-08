0c784e2f73c6d404108e027e674ff23c6af4a127
syangl <liusiyangliu@126.com>
2023-02-08 12:18:42 +0800

    temp lsy

---
+++ lsy/advanced/frontend/views/layouts/main.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Good - 互联网数据库开发TODO:小组</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="statics/img/favicon.png" rel="icon">
+  <link href="statics/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap CSS File -->
+  <link href="statics/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="statics/lib/flat-ui/css/flat-ui.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="statics/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <?= $content ?>
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        <a class="navbar-brand" href="TODO:index.html">GOOD</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="dropdown active">
+            <a href="TODO:index.html">Home</a>
+          </li>
+          <li><a href="TODO:">News</a></li>
+          <li><a href="TODO:">Questions</a></li>
+          <li><a href="TODO:">Message Board</a></li>
+          <li><a href="TODO:">Documents</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="TODO:/advanced/frontend/web/index.php?r=site/signup">Signup</a></li>
+              <li><a href="TODO:">Login</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+  </div>
+
+  <!-- Headerwrap -->
+  <div id="headerwrap-2">
+    <div class="container">
+      <div class="row">
+        <h1>Welcome to our site.<br/> <b>GOOD</b> to see you.</h1>
+      </div>
+    </div>
+  </div>
+  <!-- /Headerwrap -->
+
+  <!-- Call To Action -->
+  <div id="cta">
+    <div class="container centered">
+      <h3>Welcome to our new corporate site.</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <br>
+      
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /cta -->
+
+
+  <!-- Services Section -->
+  <div class="container sinfo">
+    <div class="row centered">
+      <h4>小组成员展示板</h4>
+      <br>
+      <br>
+      <div class="col-lg-3">
+        <p><img src="statics/img/icons/touch.svg" alt=""></p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        
+      </div>
+      <!-- col-lg-3 -->
+
+      <div class="col-lg-3">
+        <p><img src="statics/img/icons/spray.svg" alt=""></p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        
+      </div>
+      <!-- col-lg-3 -->
+
+      <div class="col-lg-3">
+        <p><img src="statics/img/icons/rocket.svg" alt=""></p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        
+      </div>
+      <!-- col-lg-3 -->
+
+      <div class="col-lg-3">
+        <p><img src="statics/img/icons/pencil.svg" alt=""></p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        
+      </div>
+      <!-- col-lg-3 -->
+
+    </div>
+    <!-- /row -->
+    <br>
+    <hr>
+  </div>
+  <!-- /container -->
+
+  
+
+
+
+
+  <!-- /Footer -->
+
+  <div id="copyrights">
+    <div class="container">
+      <p>TODO:小组制作</p>
+      <p> &copy; Copyrights <strong>Good</strong>. All Rights Reserved - <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a>
+      </p>
+      <div class="credits">
+      </div>
+    </div>
+  </div>
+  <!-- / copyrights -->
+
+  <!-- JavaScript Libraries -->
+  <script src="statics/lib/jquery/jquery.min.js"></script>
+  <script src="statics/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="statics/lib/php-mail-form/validate.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="statics/js/main.js"></script>
+
+</body>
+</html>
